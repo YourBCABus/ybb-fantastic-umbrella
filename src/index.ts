c09a5ac697e9fb3859e61aec910ee6b8fd7ae77b
Anthony Li <li.anthony917@gmail.com>
2018-10-19 11:43:04 -0400

    Add more boilerplate code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, world!");
+import fs from 'fs';
+import path from 'path';
+
+import express from 'express';
+import { json } from 'body-parser';
+import { MongoClient } from 'mongodb';
+
+const config = JSON.parse(fs.readFileSync(path.join(__dirname, "../config.json"), "utf8"));
+
+const app = express();
+app.use(json());
+
+// const client = new MongoClient(config.mongo);
+console.log(config);
