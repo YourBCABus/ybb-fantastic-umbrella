9f114c90df4e66cea02991805153713efbb7052c
Anthony Li <me@anli.dev>
2021-07-08 13:04:14 -0400

    Add comment to errorpage.ts

--- src/errorpage.ts
+++ src/errorpage.ts
@@ -1,6 +1,11 @@
 import { Application } from "express";
 import { Configuration } from "oidc-provider";
-``
+
+/**
+ * Creates a function that renders an oidc-provider error.
+ * @param app - Express application
+ * @returns a renderError function for oidc-provider
+ */
 export default function errorPage(app: Application): Configuration["renderError"] {
     return async (ctx, out, err) => {
         if (out.error === "server_error") console.error(err);
