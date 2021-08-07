54a5b45e18e4c2c3dbc18c27c15e6740ce3a6cf8
Anthony Li <me@anli.dev>
2021-08-07 19:36:26 -0400

    Spice up the error page

--- src/errorpage.ts
+++ src/errorpage.ts
@@ -1,5 +1,17 @@
 import { Application } from "express";
 import { Configuration } from "oidc-provider";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+let flavorTexts: string[] = [];
+try {
+    flavorTexts = readFileSync(join(__dirname, "../errormessages.txt"), "utf8").split("\n").map(msg => msg.trim()).filter(msg => msg.length > 0);
+} catch (e) {
+    console.error(`Error obtaining flavor texts: ${e}`);
+}
+if (flavorTexts.length === 0) {
+    flavorTexts.push("An error occurred.");
+}
 
 /**
  * Creates a function that renders an oidc-provider error.
@@ -10,7 +22,11 @@ export default function errorPage(app: Application): Configuration["renderError"
     return async (ctx, out, err) => {
         if (out.error === "server_error") console.error(err);
         ctx.body = await new Promise((res, rej) => {
-            app.render("error", { ...out, status: ctx.status }, (err, html) => {
+            app.render("error", { 
+                ...out,
+                status: ctx.status,
+                flavor: flavorTexts[Math.floor(Math.random() * flavorTexts.length)]
+            }, (err, html) => {
                 err ? rej(err) : res(html!);
             });
         });
